0d6b91e289919c242f507f0154841e98b757ea91
Biggama13 <lucasgamao.santos@gmail.com>
2025-05-09 18:08:26 -0300

    Alteração index.js

--- BACKEND/API/index.js
+++ BACKEND/API/index.js
@@ -7,14 +7,18 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-
 app.use(express.json());
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
-const dbURI = process.env.MONGO_URI;
-// Conexão com MongoDB
-mongoose.connect(dbURI);
+
+const dbURI = "mongodb+srv://GamaNaCama:biggama91817800@lucas243889.lsk3inp.mongodb.net/TarefasDB?retryWrites=true&w=majority&appName=Lucas243889";
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB:'));
